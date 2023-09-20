702610ff31d7885728c9eeb19d54e72b076a8e94
khoshekhh <145542889+khoshekhh@users.noreply.github.com>
2023-09-20 11:50:29 +0300

    Update myfunc.c

--- app/myfunc.c
+++ app/myfunc.c
@@ -27,8 +27,11 @@ int square(double a, b, c, x1, x2) {
     }
 
     if (D > 0) {
-        x1 = (-b  / (2 * a);
-    
+        x1 = (-b + sqrt(D)) / (2 * a);
+        x2 = (-b - sqrt(D)) / (2 * a);
+        return (x1, x2);
+    }
+}
 
 int val;
 
