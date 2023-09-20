26047667ec6f43e1cc03bec928c4d0a79837de50
khoshekhh <145542889+khoshekhh@users.noreply.github.com>
2023-09-20 12:37:19 +0300

    Update myfunc.c

--- app/myfunc.c
+++ app/myfunc.c
@@ -18,18 +18,19 @@ int square(double a, double b, double c) {
     
     D = b * b - 4 * a * c;
 
-    if (D < 0)
-        printf("no roots);
-
+    if (D < 0) {
+        printf("no roots");
+    }
+    
     if (D = 0) {
         x1 = -b / (2 * a);
-        return (x1);
+        printf(x1);
     }
 
     if (D > 0) {
         x1 = (-b + sqrt(D)) / (2 * a);
         x2 = (-b - sqrt(D)) / (2 * a);
-        return (x1, x2);
+        printf(x1, x2);
     }
 }
 
